1db1659e9f9a3f203db9a27ae4c253705c1f9b3d
mattbf <matthewbf8@gmail.com>
2019-04-23 15:10:56 -0600

    Fixed chessboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Knight from './Chessboard/Knight.js'
-import Square from './Chessboard/Square.js'
+import Board from './Chessboard/Board.js';
 
 function App() {
 	return (
-		<div className="App">
-			<Square black>
-				<Knight/>
-			</Square>
+		<div style={{height: '500px'}} >
+			<Board knightPosition={[4, 4]} />
 		</div>
 	)
 }
